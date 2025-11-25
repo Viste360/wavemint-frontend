79064bf14f8967857e524d672704268c44ae8726
Dustdeep <130654638+Viste360@users.noreply.github.com>
2025-11-25 13:13:06 +0100

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,1 +1,14 @@
-// api
+export const uploadVideo = async (video, audio = null, artist) => {
+  const form = new FormData();
+  form.append("video", video);
+  if (audio) form.append("audio", audio);
+  form.append("artist", artist);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_GATEWAY_URL}/upload`, {
+    method: "POST",
+    body: form,
+  });
+
+  if (!res.ok) throw new Error("Upload failed.");
+  return await res.json();
+};
