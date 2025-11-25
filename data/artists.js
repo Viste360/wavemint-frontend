dc6bc4fe4e912a90c00c161eab0fa37a2e37391f
Dustdeep <130654638+Viste360@users.noreply.github.com>
2025-11-25 13:07:59 +0100

    Update artists.js

--- data/artists.js
+++ data/artists.js
@@ -1,1 +1,34 @@
-// artists data
+const artists = [
+  {
+    name: "Dustdeep",
+    slug: "dustdeep",
+    color: "#ff8b3d",
+    image: "/artists/dustdeep.png",
+  },
+  {
+    name: "Nyral",
+    slug: "nyral",
+    color: "#3dbaff",
+    image: "/artists/nyral.jpg",
+  },
+  {
+    name: "Akasyon",
+    slug: "akasyon",
+    color: "#b33dff",
+    image: "/artists/akasyon.jpg",
+  },
+  {
+    name: "Globasso",
+    slug: "globasso",
+    color: "#44ff88",
+    image: "/artists/globasso.jpg",
+  },
+  {
+    name: "Kletron",
+    slug: "kletron",
+    color: "#ff44dd",
+    image: "/artists/kletron.jpg",
+  },
+];
+
+export default artists;
