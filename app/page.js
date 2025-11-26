8dcebc07e8bbf9ae2dbde81303e0632b747c5930
Dustdeep <130654638+Viste360@users.noreply.github.com>
2025-11-26 11:54:37 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  return <meta httpEquiv="refresh" content="0;url=/splash" />;
+}
+export default function Home() {
   redirect("/login");
 }
